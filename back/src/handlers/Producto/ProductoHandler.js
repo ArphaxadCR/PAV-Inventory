b1b28893b0e87aebc7cfdeb1a043c6425b0c4978
ArphaxadCR <arphaxad.calderon@gmail.com>
2024-04-18 13:43:54 -0600

    feat: :sparkles: crud producto
    Realiza el CRUD de la entidad producto, tambien inicia config de s3 de aws y genearar codigo de barras
    
    closes #1

---
+++ back/src/handlers/Producto/ProductoHandler.js
@@ -0,0 +1,175 @@
+const prisma = require("@prisma/client");
+
+class ProductoHandler {
+  static async post(nombre, descripcion, barcodeImageURL) {
+    try {
+      const producto = await prisma.producto.create({
+        data: {
+          nombre,
+          descripcion,
+          barcodeImageURL,
+        },
+      });
+      return { data: producto, message: "Producto creado con exito" };
+    } catch (error) {
+      return { error: error, message: "Error al crear Producto (handler)" };
+    }
+  }
+
+  static async logicDelete(id) {
+    try {
+      const producto = await prisma.producto.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (!producto) {
+        throw new Error("El producto no existe");
+      }
+
+      let updateDisabled = !producto.disabled;
+
+      await prisma.producto.update({
+        where: {
+          id,
+        },
+        data: {
+          disabled: updateDisabled,
+        },
+      });
+
+      return {
+        data: producto,
+        message: `El producto se ha ${
+          updateDisabled ? "desactivado" : "activado"
+        } exitosamente`,
+      };
+    } catch (error) {
+      return {
+        error: error,
+        message: "Error al desactivar Producto (handler)",
+      };
+    }
+  }
+
+  static async trueDelete(id) {
+    try {
+      const producto = await prisma.producto.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (!producto) {
+        throw new Error("El producto no existe");
+      }
+
+      await prisma.producto.delete({
+        where: {
+          id,
+        },
+      });
+
+      return {
+        message: "El Producto se ha eliminado exitosamente",
+      };
+    } catch (error) {
+      return {
+        error: error,
+        message: "Error al eliminar Producto (handler)",
+      };
+    }
+  }
+
+  static async getById(id) {
+    try {
+      const producto = await prisma.producto.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (!producto) {
+        throw new Error("El producto no existe");
+      }
+
+      return producto;
+    } catch (error) {
+      return {
+        error: error,
+        message: "Error al obtener Producto por id (handler)",
+      };
+    }
+  }
+
+  static async getByName(name) {
+    try {
+      const producto = await prisma.producto.findFirst({
+        where: {
+          nombre: name,
+        },
+      });
+
+      if (!producto) {
+        throw new Error("El producto no existe");
+      }
+
+      return producto;
+    } catch (error) {
+      return {
+        error: error,
+        message: "Error al obtener Producto por nombre (handler)",
+      };
+    }
+  }
+
+  static async getAll() {
+    try {
+      const productos = await prisma.producto.findMany({});
+
+      return productos;
+    } catch (error) {
+      return {
+        error: error,
+        message: "Error al obtener Productos (handler)",
+      };
+    }
+  }
+
+  static async update(id, nombre, descripcion) {
+    try {
+      const producto = await prisma.producto.findFirst({
+        where: {
+          id,
+        },
+      });
+
+      if (!producto) {
+        throw new Error("El producto no existe");
+      }
+
+      producto.update({
+        where: {
+          id,
+        },
+        data: {
+          nombre,
+          descripcion,
+        },
+      });
+
+      return {
+        data: producto,
+        message: "El Producto se ha actualizado exitosamente",
+      };
+    } catch (error) {
+      return {
+        error: error,
+        message: "Error al actualizar Producto (handler)",
+      };
+    }
+  }
+}
+
+module.exports = ProductoHandler;
